7a4378f58c2eb531a129deecaa9582fe2db662b1
Maxim Vishnevsky <mxmvshnvsk@gmail.com>
2021-07-09 21:48:26 +0400

    Added action for bin

--- bin/i18n-unused.js
+++ bin/i18n-unused.js
@@ -3,15 +3,41 @@ const { program } = require('commander');
 
 const { description, version } = require('../package.json');
 
+const { displayUnusedTranslations } = require('../dist/i18n-unused.umd');
+
 program.description(description);
 
 program.version(version, '-v --version', 'output version');
 
 program
-  .option('-du --display-unused', 'output table with unused translations')
-  .option('-mu --mark-unused', 'mark unused translations via [UNUSED]')
-  .option('-ru --remove-unused', 'remove unused translations')
-  .option('-dm --display-missed', 'output table with missed translations')
-  .option('-s --sync', 'sync translations');
+  .option('-ext --extensions [extensions...]', 'files extensions, which includes for searching (ext,ext,ext; by default: js, ts, jsx, tsx, vue)')
+  .option('-lExt --locales-extensions [localesExtensions...]', 'locales files extensions (ext,ext,ext; by default: json)')
+  .option('-src --src-path <srcPath>', 'path to source of code (path, ex. \'src\')')
+  .option('-path --locales-path <localesPath>', 'path to locales (path, ex. \'src/locales\')');
+
+program
+  .command('display-unused')
+  .description('output table with unused translations')
+  .action(() => displayUnusedTranslations(program.opts()));
+
+program
+  .command('mark-unused')
+  .description('mark unused translations via [UNUSED]')
+  .action(displayUnusedTranslations);
+
+program
+  .command('remove-unused')
+  .description('remove unused translations')
+  .action(displayUnusedTranslations);
+
+program
+  .command('display-missed')
+  .description('output table with missed translations')
+  .action(displayUnusedTranslations);
+
+program
+  .command('sync <source> [targe]')
+  .description('sync translations')
+  .action(displayUnusedTranslations);
 
 program.parse(process.argv);
