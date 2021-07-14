bb40c3b35c7d8b1441a50292eea75ab75ca0fc8e
Maxim Vishnevsky <mxmvshnvsk@gmail.com>
2021-07-14 10:47:32 +0400

    Added remove action

---
+++ src/actions/remove.ts
@@ -0,0 +1,35 @@
+import { writeFileSync } from 'fs';
+
+import { RunOptions, UnusedCollect } from '../types';
+
+import { initialize } from '../helpers/initialize';
+import { collectUnusedTranslations } from '../helpers/translations';
+import { generateLocalesPathAndCodes } from '../helpers/findLocales';
+import { applyToFlatKey } from '../helpers/action';
+
+export const removeUnusedTranslations = async (options: RunOptions): Promise<UnusedCollect> => {
+  const config = initialize(options);
+
+  const { localesFilePaths } = await generateLocalesPathAndCodes(
+    config.localesPath,
+    config.localesExtensions,
+  );
+
+  const unusedTranslationsCollect = await collectUnusedTranslations(
+    localesFilePaths,
+    `${process.cwd()}/${config.srcPath}`,
+    config.extensions,
+  );
+
+  unusedTranslationsCollect.forEach((collect) => {
+    const locale = require(collect.path);
+
+    collect.keys.forEach((key) => applyToFlatKey(locale, key, (source, lastKey) => {
+      delete source[lastKey];
+    }));
+
+    writeFileSync(collect.path, JSON.stringify(locale, null, 2));
+  });
+
+  return unusedTranslationsCollect;
+};
