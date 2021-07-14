7ce63bfa2e30450c41520066b790d35e4bca86b4
Maxim Vishnevsky <mxmvshnvsk@gmail.com>
2021-07-14 11:17:53 +0400

    Added shared collectUnusedTranslations

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { displayUnusedTranslations } from './actions/display';
 export { removeUnusedTranslations } from './actions/remove';
 export { markUnusedTranslations } from './actions/mark';
 export { syncTranslations } from './actions/sync';
+
+export { collectUnusedTranslations } from './helpers/translations';
