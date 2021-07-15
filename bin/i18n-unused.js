027754a21a2fc4ecb96253c4c1079ae11d7c1a0c
Maxim Vishnevsky <mxmvshnvsk@gmail.com>
2021-07-15 13:06:37 +0400

    Fixed inline options

--- bin/i18n-unused.js
+++ bin/i18n-unused.js
@@ -15,25 +15,25 @@ program.description(description);
 program.version(version, '-v --version', 'output version');
 
 program
-  .option('-ext --extensions [extensions...]', 'files extensions, which includes for searching (ext,ext,ext; by default: js, ts, jsx, tsx, vue)')
-  .option('-lExt --locales-extensions [localesExtensions...]', 'locales files extensions (ext,ext,ext; by default: json)')
-  .option('-src --src-path <srcPath>', 'path to source of code (path, ex. \'src\')')
-  .option('-path --locales-path <localesPath>', 'path to locales (path, ex. \'src/locales\')');
+  .option('-ext, --extensions [extensions...]', 'files extensions, which includes for searching (ext ext ext; by default: js, ts, jsx, tsx, vue)')
+  .option('-lExt, --locales-extensions [localesExtensions...]', 'locales files extensions (ext,ext,ext; by default: json)')
+  .option('-src, --src-path <srcPath>', 'path to source of code (path, ex. \'src\')')
+  .option('-path, --locales-path <localesPath>', 'path to locales (path, ex. \'src/locales\')');
 
 program
   .command('display-unused')
   .description('output table with unused translations')
-  .action(displayUnusedTranslations);
+  .action(() => displayUnusedTranslations(program.opts()));
 
 program
   .command('mark-unused')
   .description('mark unused translations via [UNUSED] or your marker from config')
-  .action(markUnusedTranslations);
+  .action(() => markUnusedTranslations(program.opts()));
 
 program
   .command('remove-unused')
   .description('remove unused translations')
-  .action(removeUnusedTranslations);
+  .action(() => removeUnusedTranslations(program.opts()));
 
 program
   .command('display-missed')
@@ -43,6 +43,6 @@ program
 program
   .command('sync source target')
   .description('sync translations')
-  .action(syncTranslations);
+  .action((s, t) => syncTranslations(s, t, program.opts()));
 
 program.parse(process.argv);
